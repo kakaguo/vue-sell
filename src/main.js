87afc0a4bffb118f776fa8aff48f2cb7d24a840f
kakaguo <guojunru1989@126.com>
2017-04-13 00:54:51 +0800

    将路由和main.js分开，代码更易读

--- src/main.js
+++ src/main.js
@@ -2,29 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
-import VueRouter from 'vue-router';
-import VueResource from 'vue-resource';
-import goods from 'components/goods/goods';
-import ratings from 'components/ratings/ratings';
-import seller from 'components/seller/seller';
-
+import router from './router';
 import 'common/stylus/index.styl';
-Vue.use(VueRouter);
+import VueResource from 'vue-resource';
+
 Vue.use(VueResource);
-
-const routes = [
-  { path: '/goods', component: goods },
-  { path: '/ratings', component: ratings },
-  { path: '/seller', component: seller },
-  { path: '/', redirect: { path: 'goods' } }
-];
-
-const router = new VueRouter({
-  routes, linkActiveClass: 'active'
-});
-
-/* eslint-disable */
-const app = new Vue({
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
   router,
-  render: h => h(App)
-}).$mount('#app');
+  template: '<App/>',
+  components: { App }
+});
